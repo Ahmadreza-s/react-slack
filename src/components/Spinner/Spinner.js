c82af22aed74fd7ef5caca48b373d34b863c150c
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-04-22 20:07:57 +0430

    add page option to Spinner Component

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Dimmer, Loader} from 'semantic-ui-react';
 
-const spinner = ({text = 'Preparing Chat ...'}) => {
+const spinner = ({text = 'Preparing Chat ...', page = false}) => {
     return (
-        <Dimmer active>
+        <Dimmer active page={page}>
             <Loader size='huge' content={text}/>
         </Dimmer>
     );
