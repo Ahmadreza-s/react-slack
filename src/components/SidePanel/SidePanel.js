052d2c087c7991ac72cb203d8145660aa7eba780
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-04-22 04:59:44 +0430

    added UserPanel to SidePanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Menu from 'semantic-ui-react';
+import {Menu} from 'semantic-ui-react';
+import UserPanel from './UserPanel/UserPanel';
 
 const SidePanel = () => {
     return (
-        <Menu>
-
+        <Menu size='large'
+              inverted
+              fixed='left'
+              vertical
+              style={{backgroundColor: '#4c3c4c', fontSize: '1.2rem'}}>
+            <UserPanel/>
         </Menu>
     );
 };
