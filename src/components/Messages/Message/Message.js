856bb08b464acf2f506ff794c177b7d03a3b56e0
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-04-22 20:37:30 +0430

    add Message Component

---
+++ src/components/Messages/Message/Message.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Comment} from 'semantic-ui-react';
+import moment from 'moment';
+
+const timeFromNow = timestamp => moment(timestamp).fromNow();
+const Message = ({message, user}) => {
+    return (
+        <Comment>
+            <Comment.Avatar src={message.user.avatar}/>
+            <Comment.Content className={message.user.id === user.id ? 'message_self' : ''}>
+                <Comment.Author as='a'>{message.user.name}</Comment.Author>
+                <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
+                <Comment.Text>{message.content}</Comment.Text>
+            </Comment.Content>
+        </Comment>
+    );
+};
+
+export default Message;
