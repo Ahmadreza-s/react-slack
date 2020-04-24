33ddf5a8d32473b57c387580c6a0f631450741ba
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-04-24 21:40:28 +0430

    add emojiPicker to MessagesForm

--- src/components/Messages/MessagesForm/MessagesForm.js
+++ src/components/Messages/MessagesForm/MessagesForm.js
@@ -3,6 +3,8 @@ import {Button, Input, Segment} from 'semantic-ui-react';
 import {sendMessage} from '../../../redux/messages/messages.actions';
 import {useDispatch, useSelector} from 'react-redux';
 import FileModal from './FileModal/FileModal';
+import {emojiIndex, Picker} from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 const MessagesForm = () => {
     const isSending = useSelector(state => state.messages.isSending);
@@ -11,6 +13,7 @@ const MessagesForm = () => {
 
     const [message, setMessage] = React.useState('');
     const [modalState, setModalState] = React.useState(false);
+    const [emojiPicker, setEmojiPicker] = React.useState(false);
 
     const inputRef = React.useRef(null);
 
@@ -36,13 +39,52 @@ const MessagesForm = () => {
 
     React.useEffect(() => inputRef.current.focus());
 
+    const selectEmojiHandler = emoji => {
+        let newMessage = message ? colonToUnicode(`${message} ${emoji.colons}`) : colonToUnicode(emoji.colons);
+        setEmojiPicker(false);
+        setMessage(newMessage);
+    };
+    const colonToUnicode = message => {
+        return message.replace(/:[A-Za-z0-9_+-]+:/g, x => {
+            x = x.replace(/:/g, '');
+            let emoji = emojiIndex.emojis[x];
+            if (typeof emoji !== 'undefined') {
+                let unicode = emoji.native;
+                if (typeof unicode !== 'undefined') {
+                    return unicode;
+                }
+            }
+            x = ':' + x + ':';
+            return x;
+        });
+    };
+
+    const moveCaretAtEnd = e => {
+        let temp_value = e.target.value;
+        e.target.value = '';
+        e.target.value = temp_value;
+    };
+
     return (
         <Segment className='message__form'>
+            {
+                emojiPicker ? (
+                    <Picker set='apple'
+                            onSelect={selectEmojiHandler}
+                            className='emojipicker'
+                            title='Pick your emoji'
+                            emoji='point_up'/>
+                ) : null
+            }
             <Input fluid
                    name='message'
                    style={{marginBottom: '.7em'}}
                    disabled={isSending}
-                   label={<Button icon='add'/>}
+                   label={<Button onClick={() => setEmojiPicker(true)}
+                                  icon={emojiPicker ? 'close' : 'add'}
+                                  content={emojiPicker ? 'Close' : null}/>}
+                   autoFocus
+                   onFocus={moveCaretAtEnd}
                    labelPosition='left'
                    placeholder='Write Your Message'
                    onChange={changeHandler}
